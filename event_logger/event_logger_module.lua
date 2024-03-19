b9fbc369d844eeafb4a1f3db404f9303b9effaa8
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-03-19 10:52:35 +0400

    new module

--- event_logger/event_logger_module.lua
+++ event_logger/event_logger_module.lua
@@ -1,3 +1,36 @@
+--- EventLogger modules allow you to track player actions
+--- Can be useful to detect first connection, or when a player does an action in your game to see the progress.
+--- It can also be used for connection streak to give rewards to a player.
+--- USAGE:
+--- Modules = {
+---    eventLogger = "github.com/caillef/cubzh-library/event_logger:a7320a5",
+--- }
+---
+--- -- Here is an example to send a Welcome message on FirstConnection
+--- LocalEvent:Listen("eventLoggerEvent", function(data)
+---		if data.type == "FirstConnection" then
+--- 		print("Welcome message")
+---     end
+--- end)
+---
+--- eventLogger:log(Player, "sessionsLog", { v = 1, date = Time.Unix() }, function(logs)
+--- 	if #logs == 1 then
+--- 		LocalEvent:Send("eventLoggerEvent", { type = "FirstConnection" })
+--- 	end
+---
+--- 	if #logs > 1 then -- not first connection
+--- 		print("Last connection", logs[#logs - 1].date)
+--- 	end
+--- end)
+---
+--- -- Maybe send a reward to the player if he played 7 days in a row?
+--- eventLogger:get(Player, "sessionsLog", function(logs)
+---		-- print all connections
+---		for index,log in ipairs(logs) do
+--- 		print("Entry", index, log.date)
+--- 	end
+--- end)
+
 local eventLogger = {}
 
 eventLogger.log = function(_, player, eventName, eventData, callback)
