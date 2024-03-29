233dfac19f927eb14c62a7723a888f330ba22059
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-03-29 10:11:58 +0400

    new module

--- ui_blocks/ui_blocks_module.lua
+++ ui_blocks/ui_blocks_module.lua
@@ -142,6 +142,38 @@ ui_blocks.createLineContainer = function(_, config)
 	return node, elems
 end
 
+ui_blocks.setNodePos = function(_, node, horizontalAnchor, verticalAnchor, margins)
+	margins = margins or 0
+	if type(margins) ~= "table" then
+		-- left, bottom, right, top
+		margins = { margins, margins, margins, margins }
+	end
+
+	local x = 0
+	local y = 0
+
+	local parentWidth = node.parent and node.parent.Width or Screen.Width
+	local parentHeight = node.parent and node.parent.Height or (Screen.Height - Screen.SafeArea.Top)
+
+	if horizontalAnchor == "left" then
+		x = margins[3]
+	elseif horizontalAnchor == "center" then
+		x = parentWidth * 0.5 - node.Width * 0.5
+	elseif horizontalAnchor == "right" then
+		x = parentWidth - margins[1] - node.Width
+	end
+
+	if verticalAnchor == "bottom" then
+		y = margins[2]
+	elseif verticalAnchor == "center" then
+		y = parentHeight * 0.5 - node.Height * 0.5
+	elseif verticalAnchor == "top" then
+		y = parentHeight - margins[4] - node.Height
+	end
+
+	node.pos = { x, y }
+end
+
 ui_blocks.anchorNode = function(_, node, horizontalAnchor, verticalAnchor, margins)
 	margins = margins or 0
 	if type(margins) ~= "table" then
